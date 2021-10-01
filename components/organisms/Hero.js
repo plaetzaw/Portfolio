365de9aa0fb959c042d77d13aa451266f15a3a6a
plaetzaw <plaetzaw@gmail.com>
2021-10-01 12:24:38 -0500

    Mostly happy with projects, need to fix about me bug

--- components/organisms/Hero.js
+++ components/organisms/Hero.js
@@ -1,28 +1,31 @@
 import Image from 'next/image'
 import Check from '../atoms/Check'
 import Github from '../atoms/GitHub'
-import Indeed from '../atoms/Indeed'
+import LinkedIn from '../atoms/LinkedIn'
 import Medium from '../atoms/Medium'
 
 const Hero = () => {
+  const Navigate = () => {
+
+  }
   return (
     <div className='bg-[#5F3C92] flex flex-col items-center justify-center text-center md:flex-row'>
       <div className='py-4 md:w-1/2'>
         <Image
           src='/AlexHS.png'
-          width={152}
-          height={156}
+          width={200}
+          height={200}
         />
       </div>
       <div className='flex flex-col items-center'>
         <span className='text-[#F2EFEF] pb-2 text-5xl'>Hi, my name is</span>
         <span className='text-[#FF7A00] text-4xl'>ALEX PLAETZER</span>
-        {/* <div className='md:hidden'><Check /></div> */}
+        <div className='animate-pulse'><Check /></div>
         <span className='text-[#F2EFEF] pt-1 text-4xl'>Full-Stack Developer</span>
         <div className='flex flex-row'>
-          <div className='px-2.5 py-4 hover:animate-spin'><Github /></div>
-          <div className='px-2.5 py-4 hover:animate-pulse'><Indeed /></div>
-          <div className='px-2.5 py-4 hover:animate-bounce'><Medium /></div>
+          <div className='px-2.5 py-4 cursor-pointer hover:animate-bounce'><Github /></div>
+          <div className='px-2.5 py-4 cursor-pointer hover:animate-bounce'><LinkedIn /></div>
+          <div className='px-2.5 py-4 cursor-pointer hover:animate-bounce'><Medium /></div>
         </div>
       </div>
 
--- components/organisms/Projects.js
+++ components/organisms/Projects.js
@@ -30,14 +30,20 @@ const Projects = () => {
     return (
       <div key={index} className='px-4 py-8 flex flex-col justify-center md:flex-row'>
         <div className='md:w-1/3'>
-          <div className='md: pr-4'><Image src={project.src} height={250} width={250} /></div>
-          <div className='flex flex-row justify-center text-[#F2EFEF]'>
-            <div onClick={() => { Navigate(project.website) }}><Website /><span>Deploy</span></div>
-            <div onClick={() => { Navigate(project.feURL) }}><Github /><span>Front-End</span></div>
-            <div onClick={() => { Navigate(project.beURL) }}><Github /><span>Back-End</span></div>
+          <div className='pb-2 md: pr-4'><Image src={project.src} height={250} width={250} /></div>
+          <div className='flex flex-row justify-center text-[#F2EFEF] text-xl pb-2 px-5'>
+            <div className='px-3 cursor-pointer hover:animate-bounce' onClick={() => { Navigate(project.website) }}><Website />
+              <span>Deployed</span>
+            </div>
+            <div className='px-3 cursor-pointer hover:animate-bounce' onClick={() => { Navigate(project.feURL) }}><Github />
+              <span>Front-End</span>
+            </div>
+            <div className='px-3 cursor-pointer hover:animate-bounce' onClick={() => { Navigate(project.beURL) }}><Github />
+              <span>Back-End</span>
+            </div>
           </div>
         </div>
-        <div className='md:w-2/3 flex-col'>
+        <div className='md:w-2/3 pr-6 flex-col'>
           <span className='text-[#F2EFEF] m-2 text-3xl'>{project.title}</span>
           <p className='text-[#707097] m-2 text-2xl'>{project.description}</p>
         </div>
